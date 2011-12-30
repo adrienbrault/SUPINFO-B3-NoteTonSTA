93dbe0cfc87004dcf3f9f3a1b66293410d9b8e3e
Adrien BRAULT <monsti@gmail.com>
2011-12-30 22:42:05 +0100

    Fix warning

--- src/fr/adrienbrault/notetonsta/servlet/CreateEvaluationServlet.java
+++ src/fr/adrienbrault/notetonsta/servlet/CreateEvaluationServlet.java
@@ -8,7 +8,6 @@ import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
