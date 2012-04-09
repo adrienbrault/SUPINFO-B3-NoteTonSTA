3c40d3b8decb0f149af2386ad9048ca0b674d7f0
Adrien BRAULT <monsti@gmail.com>
2012-04-10 00:20:26 +0200

    Evaluation resource now also return json

--- src/fr/adrienbrault/notetonsta/resource/EvaluationResource.java
+++ src/fr/adrienbrault/notetonsta/resource/EvaluationResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -23,6 +24,7 @@ public class EvaluationResource extends HibernateResource {
 
 	@GET
 	@Path("/{id}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	public Evaluation getEvaluation(@PathParam("id") Integer id) {
 		EvaluationDao evaluationDao = new EvaluationDao(createEm());
 		
